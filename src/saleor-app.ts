8f157d57948103b5f780bfb42e4c18e3ca3d745e
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-12-12 11:21:22 +0100

    fix build

--- src/saleor-app.ts
+++ src/saleor-app.ts
@@ -1,4 +1,4 @@
-import { VercelKvApl } from "@saleor/app-sdk/APL";
+import { _experimental_VercelKvApl } from "@saleor/app-sdk/APL/vercel-kv";
 import { SaleorApp } from "@saleor/app-sdk/saleor-app";
 
 /**
@@ -8,7 +8,7 @@ import { SaleorApp } from "@saleor/app-sdk/saleor-app";
  * To read more about storing auth data, read the
  * [APL documentation](https://github.com/saleor/saleor-app-sdk/blob/main/docs/apl.md)
  */
-export const apl = new VercelKvApl();
+export const apl = new _experimental_VercelKvApl();
 
 export const saleorApp = new SaleorApp({
   apl,
