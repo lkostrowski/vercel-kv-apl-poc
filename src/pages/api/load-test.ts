59d7b056f84c6c6d1dc8763a84f1a8561651ce04
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-12-12 11:30:50 +0100

    fix headers

--- src/pages/api/load-test.ts
+++ src/pages/api/load-test.ts
@@ -2,13 +2,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { saleorApp } from "../../saleor-app";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.headers.saleorApiUrl === undefined) {
+  console.log(req.headers)
+
+  if (req.headers['saleor-api-url'] === undefined) {
     return res.status(400).send({
       error: "saleorApiUrl header is required",
     });
   }
 
-  const authData = await saleorApp.apl.get(req.headers.saleorApiUrl as string);
+  const authData = await saleorApp.apl.get(req.headers['saleor-api-url'] as string);
 
   return res.send({
     url: authData?.saleorApiUrl,
